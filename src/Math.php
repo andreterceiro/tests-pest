736db8d730711720b9090f62d26790a719c91f29
André de Paula Terceiro <andreterceiro@yahoo.com.br>
2024-08-09 16:15:28 -0300

    Removing an unecessary call of Composer autoloader

--- src/Math.php
+++ src/Math.php
@@ -1,6 +1,5 @@
 <?php
 namespace Andreterceiro;
-require_once(__DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php');
 
 class Math {
     public static function subtract($a, $b) {
